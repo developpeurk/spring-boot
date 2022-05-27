d04e96baecf24773189ef4b72fb3243bbd2adc32
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-05-27 22:37:14 +0100

    redirect to customers when adding new customer

--- src/app/new-customer/new-customer.component.ts
+++ src/app/new-customer/new-customer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Customer} from "../model/customer.model";
 import {CustomerService} from "../services/customer.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-new-customer',
@@ -11,7 +12,7 @@ import {CustomerService} from "../services/customer.service";
 export class NewCustomerComponent implements OnInit {
 
   newCustomerFormGroup!: FormGroup
-  constructor(private fb: FormBuilder, private customerService: CustomerService) { }
+  constructor(private fb: FormBuilder, private customerService: CustomerService, private router: Router) { }
 
   ngOnInit(): void {
     this.newCustomerFormGroup = this.fb.group({
@@ -26,6 +27,8 @@ export class NewCustomerComponent implements OnInit {
     this.customerService.saveCustomer(customer).subscribe({
       next: data=> {
         alert("Customer has been saved successfully!");
+        //this.newCustomerFormGroup.reset()
+        this.router.navigateByUrl("/customers")
       },
       error : err => {
         console.log(err)
