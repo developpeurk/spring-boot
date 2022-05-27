9e24e49ec36966ef5b0d1a68df51a6258f4ecfb7
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-05-27 19:31:13 +0100

    other method instead of subscribe

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {CustomerService} from "../services/customer.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-customers',
@@ -8,19 +9,12 @@ import {CustomerService} from "../services/customer.service";
 })
 export class CustomersComponent implements OnInit {
 
-  public customers: any
+  public customers$!: Observable<any>
   errorMessage!: string;
   constructor(private customerService:CustomerService) { }
 
   ngOnInit(): void {
-    this.customerService.getCustomers().subscribe( {
-     next: (data)=>{
-       this.customers = data;
-     },
-      error: (err => {
-        this.errorMessage = err.message
-      })
-    })
+  this.customers$ = this.customerService.getCustomers();
   }
 
 }
