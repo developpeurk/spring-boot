a550fcd07ffb5e3d4a381d7492af1f5b252e7118
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-05-27 20:11:06 +0100

    add search

--- src/main/java/com/lambarki/yassine/ebank_lambarki_yassine/repositories/CustomerRepository.java
+++ src/main/java/com/lambarki/yassine/ebank_lambarki_yassine/repositories/CustomerRepository.java
@@ -3,5 +3,8 @@ package com.lambarki.yassine.ebank_lambarki_yassine.repositories;
 import com.lambarki.yassine.ebank_lambarki_yassine.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    List<Customer> findByNameContains(String keyword);
 }
--- src/main/java/com/lambarki/yassine/ebank_lambarki_yassine/web/CustomerRestController.java
+++ src/main/java/com/lambarki/yassine/ebank_lambarki_yassine/web/CustomerRestController.java
@@ -21,6 +21,13 @@ public class CustomerRestController {
         return bankAccountService.listCustomers();
     }
 
+
+    @GetMapping("/customers/search")
+    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword", defaultValue = "") String keyword){
+        return bankAccountService.searchCustomers(keyword);
+    }
+
+
     @GetMapping("/customers/{id}")
     public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
         return bankAccountService.getCustomer(customerId);
