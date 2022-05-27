4716d9a12e0cd3ef97077f610e46fd5931ceeb60
softengyas <y.lambarki@etu.enset-media.ac.ma>
2022-05-27 21:32:15 +0100

    search...

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {CustomerService} from "../services/customer.service";
 import {catchError, Observable, throwError} from "rxjs";
 import {Customer} from "../model/customer.model";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-customers',
@@ -12,9 +13,13 @@ export class CustomersComponent implements OnInit {
 
   public customers$!: Observable<Array<Customer>>
   errorMessage!: string;
-  constructor(private customerService:CustomerService) { }
+  searchFormGroup: FormGroup | undefined
+  constructor(private customerService:CustomerService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.searchFormGroup = this.fb.group({
+      keyword: this.fb.control("")
+    })
   this.customers$ = this.customerService.getCustomers().pipe(
     catchError(err => {
       this.errorMessage = err.message
@@ -23,4 +28,13 @@ export class CustomersComponent implements OnInit {
   );
   }
 
+  handleSearchCustomers() {
+      let kw = this.searchFormGroup?.value.keyword
+      this.customers$ = this.customerService.searchCustomers(kw).pipe(
+      catchError(err => {
+        this.errorMessage = err.message
+        return throwError(err)
+      })
+    );
+  }
 }
